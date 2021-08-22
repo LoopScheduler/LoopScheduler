7a7eeae4a024c65f1fba9ca95371348ddabaace6
Majidzadeh <hashpragmaonce@gmail.com>
2021-08-22 18:50:49 +0430

    Add to the documentation that Loop methods are thread-safe

--- LoopScheduler/Loop.h
+++ LoopScheduler/Loop.h
@@ -14,12 +14,12 @@ namespace LoopScheduler
         Loop(std::shared_ptr<Group> Architecture);
         ~Loop();
 
-        /// @brief Starts the loop. An exception will be thrown if called more than once without a Stop() in between.
+        /// @brief Thread-safe method to start the loop. An exception will be thrown if called more than once without a Stop() in between.
         /// @param threads_count The number of threads to allocate. The default is the number of logical CPU cores.
         void Start(int threads_count = 0);
-        /// @brief Stops the loop. Won't do anything if the loop isn't running.
+        /// @brief Thread-safe method to stop the loop. Won't do anything if the loop isn't running.
         void Stop();
-        /// @brief Returns whether the loop is running.
+        /// @brief Thread-safe method to check whether the loop is running.
         bool IsRunning();
     private:
         std::shared_ptr<Group> Architecture;
