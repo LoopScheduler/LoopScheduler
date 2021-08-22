a39e44e657c4cbc4defd7279e7b04351ba6a2b6b
Majidzadeh <hashpragmaonce@gmail.com>
2021-08-22 18:43:44 +0430

    Add LoopScheduler.h

---
+++ LoopScheduler/LoopScheduler.h
@@ -0,0 +1,10 @@
+#pragma once
+
+#include "Loop.h"
+#include "Group.h"
+#include "SequentialGroup.h"
+#include "ParallelGroup.h"
+#include "GroupMember.h"
+#include "Module.h"
+#include "TimeSpanPredictor.h"
+#include "BiasedEMATimeSpanPredictor.h"
