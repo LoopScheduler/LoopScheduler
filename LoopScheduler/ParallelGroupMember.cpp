c0684d3cac4d5cc1c7fe3e8b341cfb514c36f5f6
Majidzadeh <hashpragmaonce@gmail.com>
2021-09-17 17:38:31 +0430

    Update ParallelGroupMember implementation

--- LoopScheduler/ParallelGroupMember.cpp
+++ LoopScheduler/ParallelGroupMember.cpp
@@ -4,10 +4,8 @@ namespace LoopScheduler
 {
     ParallelGroupMember::ParallelGroupMember(
             std::variant<std::shared_ptr<Group>, std::shared_ptr<Module>> Member,
-            bool CanRunMoreThanOncePreCycle,
-            bool CanRunInParallel
+            bool CanRunMoreThanOncePreCycle
         ) : Member(Member),
-            CanRunMoreThanOncePreCycle(CanRunMoreThanOncePreCycle),
-            CanRunInParallel(CanRunInParallel)
+            CanRunMoreThanOncePreCycle(CanRunMoreThanOncePreCycle)
     {}
 }
