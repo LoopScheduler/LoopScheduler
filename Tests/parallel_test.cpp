6acfee480b26be8e4e430487cf81506b9ee64b26
Majidzadeh <hashpragmaonce@gmail.com>
2021-10-01 16:30:19 +0330

    Add repeat times to parallel test and fix logic

--- Tests/parallel_test.cpp
+++ Tests/parallel_test.cpp
@@ -42,72 +42,81 @@ int main()
     int count;
     int work_amount;
     int iterations_count;
+    int test_repeats;
     std::cout << "Enter the number of threads/modules: ";
     std::cin >> count;
     std::cout << "Enter the work amount for threads/modules on each iteration (a large number like 10000): ";
     std::cin >> work_amount;
     std::cout << "Enter the number of iterations: ";
     std::cin >> iterations_count;
-    std::vector<LoopScheduler::ParallelGroupMember> members;
-    for (int i = 0; i < count; i++)
+    std::cout << "Enter the number of test repeats: ";
+    std::cin >> test_repeats;
+
+    for (int repeat_number = 0; repeat_number < test_repeats; repeat_number++)
     {
-        members.push_back(
-            LoopScheduler::ParallelGroupMember(
-                std::shared_ptr<LoopScheduler::Module>(
-                    new WorkingModule(work_amount, iterations_count)
+        std::cout << "\nTest " << repeat_number << ":\n\n";
+
+        std::vector<LoopScheduler::ParallelGroupMember> members;
+        for (int i = 0; i < count; i++)
+        {
+            members.push_back(
+                LoopScheduler::ParallelGroupMember(
+                    std::shared_ptr<LoopScheduler::Module>(
+                        new WorkingModule(work_amount, iterations_count)
+                    )
                 )
-            )
-        );
-    }
-    LoopScheduler::Loop loop(std::shared_ptr<LoopScheduler::Group>(new LoopScheduler::ParallelGroup(members)));
+            );
+        }
+        LoopScheduler::Loop loop(std::shared_ptr<LoopScheduler::Group>(new LoopScheduler::ParallelGroup(members)));
 
-    auto start = std::chrono::steady_clock::now();
-    loop.Run();
-    auto stop = std::chrono::steady_clock::now();
+        auto start = std::chrono::steady_clock::now();
+        loop.Run();
+        auto stop = std::chrono::steady_clock::now();
 
-    std::chrono::duration<double> loop_scheduler_duration = stop - start;
+        std::chrono::duration<double> loop_scheduler_duration = stop - start;
 
-    std::cout << "LoopScheduler: Total time: " << loop_scheduler_duration.count() << '\n';
-    std::cout << "               Approximate iterations per second: "
-              << iterations_count / loop_scheduler_duration.count() << "\n\n";
+        std::cout << "LoopScheduler: Total time: " << loop_scheduler_duration.count() << '\n';
+        std::cout << "               Approximate iterations per second: "
+                << iterations_count / loop_scheduler_duration.count() << "\n\n";
 
-    start = std::chrono::steady_clock::now();
-    std::vector<std::thread> threads;
-    for (int i = 0; i < count; i++)
-    {
-        threads.push_back(
-            std::thread([work_amount, iterations_count] {
-                int WorkAmount = work_amount;
-                int IterationsCount = 0;
-                int IterationsCountLimit = iterations_count;
-                while (true)
-                {
-                    IterationsCount++;
-                    if (IterationsCount > IterationsCountLimit)
-                        return;
-                    else
+        start = std::chrono::steady_clock::now();
+        std::vector<std::thread> threads;
+        for (int i = 0; i < count; i++)
+        {
+            threads.push_back(
+                std::thread([work_amount, iterations_count] {
+                    int WorkAmount = work_amount;
+                    int IterationsCount = 0;
+                    int IterationsCountLimit = iterations_count;
+                    while (true)
                     {
-                        for (int i = 0; i < WorkAmount; i++)
+                        IterationsCount++;
+                        if (IterationsCount >= IterationsCountLimit)
+                            return;
+                        else
                         {
-                            for (int i = 0; i < 100; i++); // Work unit
+                            for (int i = 0; i < WorkAmount; i++)
+                            {
+                                for (int i = 0; i < 100; i++); // Work unit
+                            }
                         }
                     }
-                }
-            })
-        );
-    }
-    for (auto& thread : threads)
-    {
-        thread.join();
-    }
-    stop = std::chrono::steady_clock::now();
+                })
+            );
+        }
+        for (auto& thread : threads)
+        {
+            thread.join();
+        }
+        stop = std::chrono::steady_clock::now();
 
-    std::chrono::duration<double> threads_duration = stop - start;
+        std::chrono::duration<double> threads_duration = stop - start;
 
-    std::cout << "Threads: Total time: " << threads_duration.count() << '\n';
-    std::cout << "         Approximate iterations per second: "
-              << iterations_count / threads_duration.count() << "\n\n";
-    std::cout << "Efficiency: " << threads_duration.count() / loop_scheduler_duration.count() << '\n';
+        std::cout << "Threads: Total time: " << threads_duration.count() << '\n';
+        std::cout << "         Approximate iterations per second: "
+                << iterations_count / threads_duration.count() << "\n\n";
+        std::cout << "Efficiency: " << threads_duration.count() / loop_scheduler_duration.count() << '\n';
+    }
 
     return 0;
 }
